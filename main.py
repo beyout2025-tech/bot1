66752b11d2b7605daccde005c11455961e10cce5
beyout2025-tech <be.you.t2025@gmail.com>
2026-03-03 03:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # main.py
 import logging
 import os
+import sys
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, request
 
 from bot_core.utils.telegram_imports import (
